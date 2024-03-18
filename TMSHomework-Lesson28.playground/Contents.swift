61457fe6bba06c7eb888edba7e400156951e2fc6
Natallia Mazur <natashamazur@bk.ru>
2024-03-18 17:31:06 +0300

    Add the version of homework with two synchronized queues

--- TMSHomework-Lesson28.playground/Contents.swift
+++ TMSHomework-Lesson28.playground/Contents.swift
@@ -1,49 +1,85 @@
 import Foundation
 
 var lock = NSLock()
+var threadLock = NSLock()
 
 class BankAccount {
     var currentBalance: Decimal = 0
 
-    var balance: Decimal {
-        get {
-            let temp = currentBalance
-            return temp
-        } set {
-            currentBalance = newValue
-        }
-    }
-
     func deposit(_ value: Decimal) {
+        lock.lock()
         currentBalance += value
-        print("Пополнение счёта на сумму \(value). Текущий баланс: \(account.balance)")
+        print("Пополнение счёта на сумму \(value). Текущий баланс: \(account.currentBalance)")
+        lock.unlock()
     }
 
     func withdraw(_ value: Decimal) {
-        if balance > 0 {
+        lock.lock()
+        if value <= currentBalance {
             currentBalance -= value
-            print("Снятие со счёта суммы \(value). Текущий баланс: \(account.balance)")
+            print("Снятие со счёта суммы \(value). Текущий баланс: \(account.currentBalance)")
+        } else {
+            print("Не удалось снять сумму \(value). Недостаточно средств")
+        }
+        lock.unlock()
+    }
+}
+
+let account = BankAccount()
+
+let withdrawThread = Thread {
+    threadLock.lock()
+    account.withdraw(550.20)
+    account.withdraw(100.30)
+    threadLock.unlock()
+}
+
+let depositThread = Thread {
+    threadLock.lock()
+    account.deposit(2000.50)
+    account.deposit(1000)
+    threadLock.unlock()
+}
+
+depositThread.start()
+withdrawThread.start()
+
+// MARK: - Вариант с использованием GCD
+
+class BankAccountGCD {
+    var currentBalance: Decimal = 0
+
+    func deposit(_ value: Decimal) {
+        currentBalance += value
+        print("Пополнение счёта на сумму \(value). Текущий баланс: \(accountGCD.currentBalance)")
+    }
+
+    func withdraw(_ value: Decimal) {
+        if value <= currentBalance {
+            currentBalance -= value
+            print("Снятие со счёта суммы \(value). Текущий баланс: \(accountGCD.currentBalance)")
         } else {
             print("Не удалось снять сумму \(value). Недостаточно средств")
         }
     }
 }
 
-let account = BankAccount()
+let accountGCD = BankAccountGCD()
 
-let depositThread = Thread {
-    lock.lock()
-    account.deposit(2000.50)
-    account.deposit(1000)
-    lock.unlock()
+let withdrawalQueue = DispatchQueue(label: "com.example.withdrawalQueue")
+let depositQueue = DispatchQueue(label: "com.example.depositQueue")
+
+DispatchQueue.global().sync {
+    print("---- GCD вариант ----")
 }
 
-let withdrawThread = Thread {
-    lock.lock()
-    account.withdraw(550.20)
-    account.withdraw(100.30)
-    lock.unlock()
+depositQueue.sync {
+    accountGCD.deposit(200)
+    accountGCD.deposit(300)
 }
 
-depositThread.start()
-withdrawThread.start()
+withdrawalQueue.sync {
+    accountGCD.withdraw(600)
+    accountGCD.withdraw(500)
+}
+
